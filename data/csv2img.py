746f712384441a6e7849ca5ce41ea42c6817e598
dat <datdo@rocketmail.com>
2017-05-04 23:19:21 -0700

    Change to fit cs599 data

--- data/csv2img.py
+++ data/csv2img.py
@@ -7,39 +7,31 @@ from scipy.misc import imsave
 
 TRAIN_PATH = "train"
 VALID_PATH = "valid"
-TEST_PATH = "test"
-DATASET_PATHS = (TRAIN_PATH, VALID_PATH, TEST_PATH)
-
-
-def pixelstr2array(pixelstr):
-    img = np.array(pixelstr.split(" ")).astype(np.int32)
-    img_size = int(np.sqrt(len(img)))
-    return img.reshape((img_size, img_size))
+DATASET_PATHS = (TRAIN_PATH, VALID_PATH)
+VALID_PROP = 0.10
 
 
 if __name__ == "__main__":
-    df = pd.read_csv(sys.argv[1])
+    imgs = pd.read_csv(sys.argv[1], header=None).as_matrix()
+    targets = pd.read_csv(sys.argv[2], header=None)[0]
 
     for dataset_path in DATASET_PATHS:
         if not os.path.exists(dataset_path):
             os.mkdir(dataset_path)
 
-        for emotion in df["emotion"].unique():
-            path_emotion = os.path.join(dataset_path, str(emotion))
-            if not os.path.exists(path_emotion):
-                os.mkdir(os.path.join(path_emotion))
+        for target in targets.unique():
+            path_target = os.path.join(dataset_path, str(target))
+            if not os.path.exists(path_target):
+                os.mkdir(os.path.join(path_target))
 
-    for i, row in df.iterrows():
-        img = pixelstr2array(row["pixels"])
+    for i in range(len(targets)):
+        img = imgs[i,:].reshape((48, 48))
+        target = targets[i]
         fname = str(i)+".png"
 
-        if row["Usage"] == "Training":
-            path = os.path.join(TRAIN_PATH, str(row["emotion"]))
-        elif row["Usage"] == "PublicTest":
-            path = os.path.join(VALID_PATH, str(row["emotion"]))
-        elif row["Usage"] == "PrivateTest":
-            path = os.path.join(TEST_PATH, str(row["emotion"]))
+        if i < VALID_PROP * len(targets):
+            path = os.path.join(VALID_PATH, str(target))
         else:
-            raise("Error: Invalid Usage: {}".format(row["Usage"]))
+            path = os.path.join(TRAIN_PATH, str(target))
 
         imsave(os.path.join(path, fname), img)
