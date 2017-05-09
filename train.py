e27ca1aa38ff80131f8dc942e062dc16a8f37cc0
dat <datdo@rocketmail.com>
2017-05-09 08:40:19 -0700

    Add model checkpointer

--- train.py
+++ train.py
@@ -1,30 +1,30 @@
-from keras.callbacks import TensorBoard
+from keras.callbacks import TensorBoard, ModelCheckpoint
 from keras.preprocessing.image import ImageDataGenerator
 from models.cnn import Inception, ResNet, VGG
 
-
 IMG_SIZE = 200
 
-
 train_datagen = ImageDataGenerator(
-    rescale=1./255,
+    rescale=1. / 255,
     horizontal_flip=True,
-    width_shift_range=0.1,
-    height_shift_range=0.1,
-    # rotation_range=15,
-    )
-test_datagen = ImageDataGenerator(rescale=1./255)
+    width_shift_range=0.10,
+    height_shift_range=0.10,
+    zoom_range=0.10,
+)
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(
-    "data/train", target_size=(IMG_SIZE, IMG_SIZE), batch_size=8, class_mode="categorical", shuffle=True)
+    "data/train", target_size=(IMG_SIZE, IMG_SIZE), batch_size=32, class_mode="categorical", shuffle=True)
 
 validation_generator = test_datagen.flow_from_directory(
-    "data/valid", target_size=(IMG_SIZE, IMG_SIZE), batch_size=8, class_mode="categorical", shuffle=False)
-
+    "data/valid", target_size=(IMG_SIZE, IMG_SIZE), batch_size=1, class_mode="categorical", shuffle=False)
 
 if __name__ == "__main__":
-    cnn = ResNet(3, lr=0.001, dropout=0.0, decay=0.001)
-    callbacks = [TensorBoard()]
+    cnn = ResNet(3, lr=0.001, dropout=0.3, decay=0.001)
+    print(cnn.model.summary())
+    callbacks = [
+        TensorBoard(), ModelCheckpoint("models", monitor="val_loss", save_best_only=True, save_weights_only=True)
+    ]
     cnn.model.fit_generator(
         train_generator,
         steps_per_epoch=train_generator.n,
@@ -32,4 +32,3 @@ if __name__ == "__main__":
         validation_data=validation_generator,
         validation_steps=validation_generator.n,
         callbacks=callbacks)
-    cnn.model.save_weights("models/weights.h5")
