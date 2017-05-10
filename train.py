d48c3ee1f3eea4d5922d5c046466815e1228907c
dat <datdo@rocketmail.com>
2017-05-10 15:55:41 -0700

    Remove save_augmented

--- train.py
+++ train.py
@@ -1,35 +1,48 @@
-from keras.callbacks import TensorBoard, ModelCheckpoint
+from keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping
 from keras.preprocessing.image import ImageDataGenerator
-from models.cnn import CNN, Inception_FCN
+from models.cnn import VGG, Inception_FCN
 
 IMG_SIZE = 48
+BATCH_SIZE = 256
+EPOCHS = 200
 
 train_datagen = ImageDataGenerator(
-    rescale=1./255,
+    rescale=1. / 255,
     horizontal_flip=True,
     width_shift_range=0.10,
     height_shift_range=0.10,
-    zoom_range=0.10,
-)
-test_datagen = ImageDataGenerator(rescale=1./255)
+    zoom_range=0.10)
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(
-    "data/train", target_size=(IMG_SIZE, IMG_SIZE), color_mode="grayscale", batch_size=32, class_mode="categorical", shuffle=True)
+    "data/train",
+    target_size=(IMG_SIZE, IMG_SIZE),
+    color_mode="grayscale",
+    batch_size=BATCH_SIZE,
+    class_mode="categorical",
+    shuffle=True)
 
 validation_generator = test_datagen.flow_from_directory(
-    "data/valid", target_size=(IMG_SIZE, IMG_SIZE), color_mode="grayscale", batch_size=1, class_mode="categorical", shuffle=False)
+    "data/valid",
+    target_size=(IMG_SIZE, IMG_SIZE),
+    color_mode="grayscale",
+    batch_size=BATCH_SIZE,
+    class_mode="categorical",
+    shuffle=False)
 
 if __name__ == "__main__":
-    # cnn = CNN(3, (48, 48, 1), lr=0.001, dropout=0.4, decay=0.001)
-    cnn = Inception_FCN(3, (48, 48, 1), lr=0.001, dropout=0.1, decay=0.001)
+    # cnn = VGG(3, (48, 48, 1), lr=0.001, dropout=0.5, decay=0.0001)
+    cnn = Inception_FCN(3, (48, 48, 1), lr=0.001, dropout=0.4, decay=0.001)
     print(cnn.model.summary())
     callbacks = [
-        TensorBoard(), ModelCheckpoint("models/weights.h5", monitor="val_loss", save_best_only=True, save_weights_only=True)
+        TensorBoard(), 
+        ModelCheckpoint("models/weights.h5", monitor="val_acc", save_best_only=True, save_weights_only=True),
+        EarlyStopping(monitor="val_acc", patience=20)
     ]
     cnn.model.fit_generator(
         train_generator,
-        steps_per_epoch=train_generator.n,
-        epochs=10,
+        steps_per_epoch=train_generator.n // BATCH_SIZE + 1,
+        epochs=EPOCHS,
         validation_data=validation_generator,
-        validation_steps=validation_generator.n,
+        validation_steps=validation_generator.n // BATCH_SIZE + 1,
         callbacks=callbacks)
